733905e533204bafa45ce1817fecd6e809392532
kw525 <73870552+kwC525@users.noreply.github.com>
2021-07-05 14:09:01 +0800

    Initial commit

---
+++ app/src/main/java/com/android/example/colormyviews/MainActivity.kt
@@ -0,0 +1,93 @@
+package com.android.example.colormyviews
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
+/**
+ * The colorMyViews app demonstrates how to use a ConstraintLayout using
+ * the Layout Editor.
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setListeners()
+    }
+
+    /**
+     * Attaches listeners to all the views.
+     */
+    private fun makeColored(view: View,box_three_text: View, box_four_text: View, box_five_text: View) {
+
+        when (view.id) {
+
+            // Boxes using Color class colors for background
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+
+            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
+            R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+
+            // Boxes using custom colors for background
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
+    }
+    private fun setListeners() {
+
+        val boxOneText = findViewById<TextView>(R.id.box_one_text)
+        val boxTwoText = findViewById<TextView>(R.id.box_two_text)
+        val boxThreeText = findViewById<TextView>(R.id.box_three_text)
+        val boxFourText = findViewById<TextView>(R.id.box_four_text)
+        val boxFiveText = findViewById<TextView>(R.id.box_five_text)
+
+        val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
+
+        val redButton = findViewById<Button>(R.id.red_button)
+        val greenButton = findViewById<Button>(R.id.green_button)
+        val yellowButton = findViewById<Button>(R.id.yellow_button)
+
+        //redButton.setOnClickListener {
+         //   boxThreeText.setBackgroundResource(R.color.my_red)
+       // }
+
+        //greenButton.setOnClickListener {
+        //    boxFourText.setBackgroundResource(R.color.my_green)
+        //}
+
+        //yellowButton.setOnClickListener {
+        //    boxFiveText.setBackgroundResource(R.color.my_yellow)
+        //}
+
+
+        val clickableViews: List<View> =
+            listOf(
+                boxOneText, boxTwoText, boxThreeText,
+                boxFourText, boxFiveText, rootConstraintLayout,
+                redButton, greenButton, yellowButton
+            )
+
+        for (item in clickableViews) {
+            item.setOnClickListener { makeColored(it,boxThreeText, boxFourText, boxFiveText ) }
+        }
+    }
+
+    /**
+     * Sets the background color of a view depending on it's resource id.
+     * This is a way of using one click handler to do similar operations on a
+     * group of views.
+     */
+
+
+}
